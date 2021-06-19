cb2efaf7136bace30e8ea0df13f4401cb1ede078
Niccolò Mantovani <nicomanto49@gmail.com>
2021-06-19 22:26:47 +0200

    start index page

--- i18n/config.ts
+++ i18n/config.ts
@@ -5,10 +5,12 @@ import errorEN from "./en/pages/error.json";
 import formEmailEN from "./en/components/formEmail.json";
 import layoutEN from "./en/components/layout.json";
 import cookieEN from "./en/components/cookie.json";
+import indexEN from "./en/pages/index.json";
 import errorIT from "./it/pages/error.json";
 import formEmailIT from "./it/components/formEmail.json";
 import layoutIT from "./it/components/layout.json";
 import cookieIT from "./it/components/cookie.json";
+import indexIT from "./it/pages/index.json";
 
 const resources = {
   en: {
@@ -16,12 +18,14 @@ const resources = {
     formEmail: formEmailEN,
     layout: layoutEN,
     cookie: cookieEN,
+    index: indexEN,
   },
   it: {
     error: errorIT,
     formEmail: formEmailIT,
     layout: layoutIT,
     cookie: cookieIT,
+    index: indexIT,
   },
 };
 
@@ -29,7 +33,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    ns: ["home", "contacts", "projects", "experience", "error", "formEmail", "layout", "cookie"],
+    ns: ["index", "contacts", "projects", "experience", "error", "formEmail", "layout", "cookie"],
     resources,
     fallbackLng: "en",
     supportedLngs: ["en", "it"],
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,52 @@
+import { Container, Row, Col, Badge } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import { SiInstagram } from "react-icons/si";
 import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-  </Layout>
-);
+const IndexPage = () => {
+  const { t } = useTranslation(["index"]);
+
+  return (
+    <Layout title="Home | Niccolò Mantovani">
+      <Container>
+        <Row>
+          <Col md={4} className="text-center">
+            <img
+              className="profilePicture"
+              src="https://www.gravatar.com/avatar/cceb51c1797bca97b7cd5211907dd744?s=1000"
+              alt="Niccolò Mantovani profile"
+            />
+            <h1 lang="it">Niccolò Mantovani</h1>
+            <Badge pill variant="info">
+              Sviluppatore Software
+            </Badge>
+
+            <ul className="list-inline mx-auto justify-content-center">
+              <li className="list-inline-item">
+                <i>
+                  <SiInstagram />
+                </i>
+              </li>
+              <li className="list-inline-item">
+                <i>
+                  <SiInstagram />
+                </i>
+              </li>
+              <li className="list-inline-item">
+                <i>
+                  <SiInstagram />
+                </i>
+              </li>
+            </ul>
+          </Col>
+          <Col md>
+            <h1>{t("title")}</h1>
+            <p>{t("biography")}</p>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
 
 export default IndexPage;
