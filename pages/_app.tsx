b40a4cfef6d37a8d9210f5bd5323e3665e3ec79e
Niccolò Mantovani <nicomanto49@gmail.com>
2021-06-28 08:59:02 +0200

    close #1 - change background color of button in cookie consent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ const PersonalWebsite = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Component {...pageProps} />
-      <CookieConsent style={{ background: "#212529" }} buttonText={t("button")}>
+      <CookieConsent style={{ background: "#212529" }} buttonText={t("button")} buttonStyle={{ background: "white"}}>
         {t("message")}
       </CookieConsent>
     </>
