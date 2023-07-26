9f4d0237afb90cffacd83e70b85941a601f8401a
nicomanto <nicomanto49@gmail.com>
2023-07-26 12:13:00 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import CookieConsent from "react-cookie-consent";
 import i18n from "../i18n/config";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
-import { Analytics } from '@vercel/analytics/react';
 
 const PersonalWebsite = ({ Component, pageProps }: AppProps) => {
   const { t } = useTranslation(["cookie"]);
